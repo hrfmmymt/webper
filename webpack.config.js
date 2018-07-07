9363c01605c4a2d19b6456eb18211f89913ca964
hrfmmymt <hrfmmymt@gmail.com>
2018-07-07 13:59:45 +0900

    rm sas

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,9 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: ['./src/index.js', './src/style.scss'],
+  entry: ['./src/index.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist/js')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].css',
-              outputPath: '../css'
-            }
-          },
-          {
-            loader: 'extract-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
   }
 }
